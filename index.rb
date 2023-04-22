1b054c183dc42492fc90ba51befedb3b398b42c2
SharpeimQ <SharpHaiyan@gmail.com>
2023-04-22 15:55:20 -0400

    Add append method

--- index.rb
+++ index.rb
@@ -2,17 +2,44 @@
 
 # linked list class
 class LinkedList
+  attr_accessor :value, :list
+
+  def initialize(value = nil)
+    if value.nil?
+      @head = nil
+    else
+      @head = Node.new(value)
+    end
+  end
+
+  def inspect_list
+    p @head.pointer
+  end
+
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value)
+    else
+      @head.pointer = Node.new(value)
+    end
+  end
 end
 
 # nodes for linked lists
 class Node
-  attr_accessor :value
+  attr_accessor :data, :pointer
 
-  def initialize(value, point = nil)
-    @value = value
-    @point = point
+  def initialize(data = nil, pointer = nil)
+    @data = data
+    @pointer = pointer
+  end
+
+  def inspect_value
+    puts "#{data} -> #{pointer}"
   end
 end
 
-test_node = Node.new('5')
-puts test_node.value
+linked = LinkedList.new(5)
+linked.inspect_list
+linked.append(19)
+linked.inspect_list
